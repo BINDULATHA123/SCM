97b3def61af7a54bb36ac98d7e5b4dc22b42f065
BINDULATHA123 <51905211+BINDULATHA123@users.noreply.github.com>
2023-04-04 16:50:02 +0530

    added scm producer

---
+++ Scmlite_Producer/src/main/java/com/example/demo/ScmliteProducerApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+import com.example.demo.config.Producerconfig;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
+import org.json.JSONException;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+@SpringBootApplication
+public class ScmliteProducerApplication {
+
+	public static void main(String[] args)throws JSONException, UnknownHostException, IOException {
+		Producerconfig producer = new Producerconfig();
+//		FileReader fileReader = new FileReader("src/main/resources/application.properties");
+//
+//		Properties properties = new Properties();
+
+		//properties.load(fileReader);
+		ClassPathResource classPathResource = new ClassPathResource("application.properties");
+		 InputStream inputStream = classPathResource.getInputStream();
+		 Properties properties = new Properties();
+		 properties.load(inputStream);
+
+    	producer.Client1(properties);
+	}
+
+}
